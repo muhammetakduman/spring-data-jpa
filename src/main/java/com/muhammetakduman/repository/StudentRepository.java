30ccb2ada4d23d2b80194f12937452436334d5ac
Muhammet Akduman <akdumanmxc@gmail.com>
2025-06-22 02:42:49 +0300

    create interface

--- src/main/java/com/muhammetakduman/repository/StudentRepository.java
+++ src/main/java/com/muhammetakduman/repository/StudentRepository.java
@@ -2,7 +2,10 @@ package com.muhammetakduman.repository;
 
 import com.muhammetakduman.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface StudentRepository extends JpaRepository<Student,Integer> {
 
 }
--- src/main/java/com/muhammetakduman/services/impl/StudentServiceİmpls.java
+++ src/main/java/com/muhammetakduman/services/impl/StudentServiceİmpls.java
@@ -2,5 +2,5 @@ package com.muhammetakduman.services.impl;
 
 import com.muhammetakduman.services.IStudentServices;
 
-public class StudentServiceİmpls extends IStudentServices {
+public class StudentServiceİmpls implements IStudentServices {
 }
