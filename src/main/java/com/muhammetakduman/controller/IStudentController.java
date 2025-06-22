769fc573551117128c7f27381dcbf2b203a2c72a
Muhammet Akduman <akdumanmxc@gmail.com>
2025-06-22 16:59:46 +0300

    postMapping in JPA

--- src/main/java/com/muhammetakduman/controller/IStudentController.java
+++ src/main/java/com/muhammetakduman/controller/IStudentController.java
@@ -1,5 +1,9 @@
 package com.muhammetakduman.controller;
 
+import com.muhammetakduman.entities.Student;
+
 public interface IStudentController {
+
+    public Student saveStudent(Student student);
 
 }
