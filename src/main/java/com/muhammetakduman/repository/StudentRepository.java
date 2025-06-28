4ba7babf88162c41128b24bf2d2399fda8987541
Muhammet Akduman <akdumanmxc@gmail.com>
2025-06-28 13:01:18 +0300

    update sql

--- src/main/java/com/muhammetakduman/repository/StudentRepository.java
+++ src/main/java/com/muhammetakduman/repository/StudentRepository.java
@@ -16,4 +16,10 @@ public interface StudentRepository extends JpaRepository<Student,Integer> {
     // YAZARKEN DB DEKİ TABLE DEĞİL DE CLASSLER VE FİELDLER KULLANILIR.
     @Query(value = "from Student" , nativeQuery = false)
     List<Student> findAllStudents();
+
+    //eğer sql kullanamk istersek nativeQuery true çekmemiz gerekmektedir.
+    //@Query(value = "selecet * from student" , nativeQuery = true)
+    //List<Student> findAllStudents;
+
+
 }
