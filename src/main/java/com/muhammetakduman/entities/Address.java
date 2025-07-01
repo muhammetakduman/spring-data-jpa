3e02c4158d0d3de260bfdf23b000bec880d87bd2
Muhammet Akduman <akdumanmxc@gmail.com>
2025-07-02 01:44:05 +0300

    OneToOny , servisi yazılıcak

---
+++ src/main/java/com/muhammetakduman/entities/Address.java
@@ -0,0 +1,25 @@
+package com.muhammetakduman.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToOne(mappedBy = "address")  //ilişki
+    private Customer customer;
+}
---
+++ src/main/java/com/muhammetakduman/entities/Customer.java
@@ -0,0 +1,27 @@
+package com.muhammetakduman.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne //ilişkiyi sağlayan
+    private Address address;
+
+}
