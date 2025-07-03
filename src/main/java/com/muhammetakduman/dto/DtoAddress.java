ceeab7f0b036ac4c671a4413247b2db537fe2c14
Muhammet Akduman <akdumanmxc@gmail.com>
2025-07-04 01:27:57 +0300

    Eğer null değeri dönmek istemezsen fronda @JsonInclude anotasyonu entegre edeceğiz.

--- src/main/java/com/muhammetakduman/dto/DtoAddress.java
+++ src/main/java/com/muhammetakduman/dto/DtoAddress.java
@@ -1,5 +1,6 @@
 package com.muhammetakduman.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- src/main/java/com/muhammetakduman/dto/DtoCustomer.java
+++ src/main/java/com/muhammetakduman/dto/DtoCustomer.java
@@ -1,5 +1,6 @@
 package com.muhammetakduman.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,10 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
+/// null dönüyorsa fronta verme.
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
+
 public class DtoCustomer {
     private Long id;
     private String name;
