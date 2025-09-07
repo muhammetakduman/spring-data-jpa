8230a4654d34b236034eed8a977b2a60655e0630
Muhammet Akduman <akdumanmxc@gmail.com>
2025-09-07 14:05:27 +0300

    PropertySoruce annotation

--- src/main/java/com/muhammetakduman/starter/SpringDataJpaApplication.java
+++ src/main/java/com/muhammetakduman/starter/SpringDataJpaApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.muhammetakduman"})
 @ComponentScan(basePackages = {"com.muhammetakduman"})
 @EnableJpaRepositories(basePackages = {"com.muhammetakduman"})
+@PropertySource(value = "classpath:app.properties")
 
 public class SpringDataJpaApplication {
 
